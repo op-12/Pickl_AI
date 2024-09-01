9ab5d57526672b58b89b087ac3bd58f1cbd46346
hello <148437780+op-12@users.noreply.github.com>
2024-09-01 18:31:04 +0530

    mark 3

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ svm_1 = joblib.load('svm_pipeline.joblib')
 etc_1 = joblib.load('etc_1.joblib')
 adb_1 = joblib.load('abc_1.joblib')
 lr_1 = joblib.load('lr_1.joblib')
-# sgd_1 = joblib.load('SGD_Classifier.joblib')
+sgd_1 = joblib.load('sgd_1.joblib')
 knn_1 = joblib.load('knn_1.joblib')
 rfc_2 = joblib.load('rfc_2.joblib')
 svm_2 = joblib.load('svm_2.joblib')
@@ -20,7 +20,7 @@ st.set_page_config(page_title = "LoanCompass🔍",
 st.title("Personalized Lending for the Digital Age 📱 [Enter the details below]")
 st.divider()
 
-model_selected = st.selectbox("Select hyperparameter tuned model", ["rfc", "svm", "etc", "adb", "sgd", "lr", "knn", "xgb | on modified data", "rfc | on modified data", "svm | on modified data", "ETC | on modified data", "abc | on modified data"])
+model_selected = st.selectbox("Select ML | Machine Learning model", ["rfc", "svm", "etc", "adb", "sgd", "lr", "knn", "xgb | on modified data", "rfc | on modified data", "svm | on modified data", "ETC | on modified data", "abc | on modified data"])
 pin = st.select_slider("Enter Pin Code", [110001, 110014, 110003, 110004, 110011])
 age = st.slider("Enter Age🧓👶", 21, 65, value=64)
 Fam = st.select_slider("Enter Number of members in your family", [1, 2, 3, 4])
@@ -60,8 +60,8 @@ def predict():
     elif model_selected == 'adb':
             model = adb_1
 
-#     elif model_selected == 'sgd':
-#             model = sgd_1
+    elif model_selected == 'sgd':
+            model = sgd_1
 
     elif model_selected == 'knn':
           model = knn_1
@@ -80,9 +80,6 @@ def predict():
     
     elif model_selected == 'ETC | on modified data':
             model = ETC_2
-
-#     elif model_selected == 'gbc | on modified data':
-#             model = gbc_2
     
     elif model_selected == 'abc | on modified data':
             model = abc_2
